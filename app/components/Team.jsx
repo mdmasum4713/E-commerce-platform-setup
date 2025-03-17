33332e0ef07aa4863eede0a9500dab9e77adb88f
mdmasum4713 <mdmasum4713@gmail.com>
2025-03-17 23:20:52 +0600

    my name added

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -5,6 +5,7 @@ export const Team = () => {
     <div>
       <div className='text-center bg-transparent'>
   <h1>Hello word</h1>
+  <h1>my name is masum</h1>
   <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio natus qui reprehenderit ducimus eum et, ut aliquid impedit atque itaque necessitatibus hic molestiae laudantium dicta magnam cupiditate, asperiores dignissimos porro.</h3>
       </div>
     </div>
