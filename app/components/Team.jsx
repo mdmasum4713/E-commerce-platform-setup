1cfd0389226d7dd07a859b5129cc3da05b53a27a
mdmasum4713 <mdmasum4713@gmail.com>
2025-04-05 22:16:50 +0600

    simle

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -11,6 +11,7 @@ export const Team = () => {
   <h1>hello tumi kmn aso</h1>
   <h1>what are you doing</h1>
   <h1>what do you do</h1>
+  <h1>i do my work</h1>
   <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio natus qui reprehenderit ducimus eum et, ut aliquid impedit atque itaque necessitatibus hic molestiae laudantium dicta magnam cupiditate, asperiores dignissimos porro.</h3>
       </div>
     </div>
