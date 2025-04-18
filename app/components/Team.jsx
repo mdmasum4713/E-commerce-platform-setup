98ed5c1b838f12f21e76f7516ead1c3ec4e35195
mdmasum4713 <mdmasum4713@gmail.com>
2025-04-18 21:56:44 +0600

    hello

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -13,6 +13,7 @@ export const Team = () => {
   <h1>what do you do</h1>
   <h1>i do my work</h1>
   <h2>in sha allah</h2>
+  <h2>igrh</h2>
   <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio natus qui reprehenderit ducimus eum et, ut aliquid impedit atque itaque necessitatibus hic molestiae laudantium dicta magnam cupiditate, asperiores dignissimos porro.</h3>
       </div>
     </div>
