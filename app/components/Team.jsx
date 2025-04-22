0462801570600494abc5aa34ebe38456e618a973
mdmasum4713 <mdmasum4713@gmail.com>
2025-04-22 16:34:13 +0600

    some text added

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -16,6 +16,7 @@ export const Team = () => {
   <h2>igrh</h2>
   <h2>kmn aso</h2>
   <h1>how are you all</h1>
+  <h1>all fine . can you give me </h1>
   <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias quae alias incidunt sed cumque magni. Nesciunt sapiente, suscipit nobis nam porro sunt dolores, dignissimos ducimus ullam tenetur earum corporis!</h1>
   <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio natus qui reprehenderit ducimus eum et, ut aliquid impedit atque itaque necessitatibus hic molestiae laudantium dicta magnam cupiditate, asperiores dignissimos porro.</h3>
       </div>
