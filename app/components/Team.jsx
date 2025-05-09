9fd1ff25272aebc0d8ee8b9635b93cddf5041fef
mdmasum4713 <mdmasum4713@gmail.com>
2025-05-09 20:35:58 +0600

    helo

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -7,22 +7,7 @@ export const Team = () => {
   <h1>Hello word</h1>
   <h1>my name is masum</h1>
   <h1>how are you</h1>
-  <h1>i am fine</h1>
-  <h1>hello tumi kmn aso</h1>
-  <h1>what are you doing</h1>
-  <h1>what do you do</h1>
-  <h1>i do my work</h1>
-  <h2>in sha allah</h2>
-  <h2>igrh</h2>
-  <h2>kmn aso</h2>
-  <h1>how are you all</h1>
-  <h1>all fine . can you give me </h1>
-  <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, dolorem.</h1>
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eum aperiam beatae, tempore minima libero.</h1>
-  <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis modi laudantium nihil blanditiis amet?</h1>
-  <h1>my 1st plane my need a job and after i go to germany</h1>
-  <h1>what are doing</h1>
-  <h1>i am setting</h1>
+ <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, quae.</h1>
       </div>
     </div>
   )
