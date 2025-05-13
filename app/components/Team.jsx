5b310bb47a502750e83f5fae11a2aac27abc93b9
mdmasum4713 <mdmasum4713@gmail.com>
2025-05-13 21:26:39 +0600

    hello

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -7,6 +7,7 @@ export const Team = () => {
   <h1>Hello word</h1>
   <h1>my name is masum</h1>
   <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium officiis ipsa corrupti nulla ullam?</h1>
+  <h5>hello sanandubari college </h5>
       </div>
     </div>
   )
