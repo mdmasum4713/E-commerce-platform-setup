e1d64878dcb1c5f28abe5239d121bbe5346a809e
mdmasum4713 <mdmasum4713@gmail.com>
2025-05-21 21:49:39 +0600

    h

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -6,6 +6,7 @@ export const Team = () => {
       <div className='text-center bg-transparent'>
   <h1>Hello word</h1>
 <h1>hello my dear developer </h1>
+<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea deserunt similique recusandae.</h1>
       </div>
     </div>
   )
