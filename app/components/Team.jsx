4e73483a676795cdc087f0a49c9f94d525ce26f3
mdmasum4713 <mdmasum4713@gmail.com>
2025-05-22 20:57:39 +0600

    hi

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -7,6 +7,7 @@ export const Team = () => {
   <h1>Hello word</h1>
 <h1>hello my dear developer </h1>
 <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea deserunt similique recusandae.</h1>
+<h1>Lorem ipsum dolor sit amet.</h1>
       </div>
     </div>
   )
