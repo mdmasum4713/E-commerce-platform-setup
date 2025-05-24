7549a209800a2001edc326a611e73170c173f645
mdmasum4713 <mdmasum4713@gmail.com>
2025-05-24 23:56:35 +0600

    word

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -6,8 +6,7 @@ export const Team = () => {
       <div className='text-center bg-transparent'>
   <h1>Hello word</h1>
 <h1>hello my dear developer </h1>
-<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea deserunt similique recusandae.</h1>
-<h1>Lorem ipsum dolor sit amet.</h1>
+<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat recusandae aliquam est id necessitatibus nihil deleniti, deserunt adipisci error enim. Consequuntur, quisquam tempora. Dolorum facilis unde illo. Maxime, nulla quam.</h1>
       </div>
     </div>
   )
