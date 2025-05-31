af0b5d9b76cfa98647775136502f934d5cb036db
mdmasum4713 <mdmasum4713@gmail.com>
2025-05-31 22:26:47 +0600

    hi

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -4,6 +4,7 @@ export const Team = () => {
   return (
     <div>Team
       <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, iure.</h1>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In pariatur facere at.</h1>
     </div>
   )
 }
