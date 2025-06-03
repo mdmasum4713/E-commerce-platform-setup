4131c7ecd623ea780535215ca3fc00219d0b7c9d
mdmasum4713 <mdmasum4713@gmail.com>
2025-06-03 20:29:41 +0600

    heder

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -5,6 +5,11 @@ export const Team = () => {
     <div>Team
       <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, iure.</h1>
       <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In pariatur facere at.</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <h6>h6</h6>
     </div>
   )
 }
