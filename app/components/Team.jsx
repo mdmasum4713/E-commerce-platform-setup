561ad80545d07d3e2f172deb2ef487a6fe46ce9e
mdmasum4713 <mdmasum4713@gmail.com>
2025-08-12 21:49:58 +0600

    lorem

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -3,33 +3,7 @@ import React from 'react'
 export const Team = () => {
   return (
     <div>Team
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, iure.</h1>
-      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In pariatur facere at.</h1>
-      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, delectus!</h2>
-      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sapiente suscipit eius maiores deserunt delectus quaerat tempora, laudantium explicabo tenetur, esse voluptatum iste odio! Impedit nobis magni mollitia rem sequi.</h3>
-      <h4>lorem 12344</h4>
-      <h5>lorem 4958395984</h5>
-      <h6>Lorem ipsum dolor sit amet.</h6>
-      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, sint.
-        
-      </h2>
-      <h1>hello my all team </h1>
-      <h1>how are you my team</h1>
-      <h1>what do you do</h1>
-      <h1>i am setting now</h1>
-      <h1>what do you say</h1>
-      <h1>i watching video</h1>
-      <h1>end my watching</h1>
-      <h1>ok no problem</h1>
-      <h1>its ok bro</h1>
-      <h1>thasts it tray to alwas</h1>
-      <h1>ok i tra this</h1>
-      <h1>no problem brother you tray</h1>
-      <h1>ok i waiting for you</h1>
-      <h1>fainaly finshed the video</h1>
-      <h1>ok its good</h1>
-      <h1>i tray alwas</h1>
-      <h1>please you give me right suggestion</h1>
+     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, porro.</h1>
     </div>
   )
 }
