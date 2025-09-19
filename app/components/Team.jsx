e6e40a64c477bd5897820ac401607ce32ce25209
mdmasum4713 <mdmasum4713@gmail.com>
2025-09-19 21:16:48 +0600

    lorem

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -7,7 +7,7 @@ export const Team = () => {
      <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, recusandae, maxime exercitationem velit placeat, natus saepe officia cum eaque vel adipisci ut nam ipsa eligendi ipsum fuga a autem soluta?</h4>
      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, distinctio.</h1>
      <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-     <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt ipsum illo cupiditate veritatis iste quae eaque nobis eveniet architecto dolore! Laborum dicta aliquid atque laboriosam obcaecati non ducimus fugiat? Modi.</h2>
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officiis, iure fugiat consequatur quaerat laudantium nulla accusantium nobis totam magni et, ex praesentium tempore perspiciatis non nemo unde, natus voluptates.</h1>
     </div>
   )
 }
